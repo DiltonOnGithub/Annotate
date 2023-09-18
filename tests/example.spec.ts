dcac52124846dfe95574fdaeaade48c84573813f
DiltonOnGithub <143005439+DiltonOnGithub@users.noreply.github.com>
2023-09-18 04:57:30 -0400

    ok tested..

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,30 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('Quora Title', async({ page })=>{
+  await page.goto('https://www.quora.com/about');
+
+  await expect(page).toHaveTitle(/About/);
+});
+
+test('Quora About', async({ page })=>{
+  await page.goto('https://www.quora.com');
+
+  await page.getByRole('link', { name: 'About' }).click();
+});
+
+test('Quora Login', async({ page })=>{
+  await page.goto('https://www.quora.com');
+  await page.getByRole('textbox',{ name : 'email'}).fill('diltondsouza1@gmail.com');
+  await page.getByRole('textbox',{ name : 'password'}).fill('Diltonlovesquora@1098#');
+  await page.getByRole('button',{ name : 'Login'}).hover();
+  await page.getByRole('button',{ name : 'Login'}).click();
+  await page.getByRole('link',{name:'Following'}).hover();
+  await page.getByRole('link',{name:'Following'}).click();
+  await page.getByRole('link',{name:'Notifications'}).hover();
+  await page.getByRole('link',{name:'Notifications'}).click();
+  await page.getByRole('combobox',{ name : 'search'}).fill('Elon Musk')
+  await page.keyboard.press('Enter');
+  await page.getByRole('link',{name:'Following'}).hover();
+});
---
+++ tests/recorded.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.quora.com/');
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('diltondsouza1@gmail.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Diltonlovesquora@1098#');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByLabel('Spaces').getByRole('img').click();
+  await page.getByPlaceholder('Search Quora').click();
+  await page.getByPlaceholder('Search Quora').fill('Elon Musk');
+  await page.getByPlaceholder('Search Quora').press('Enter');
+  await page.locator('.q-inlineFlex > div:nth-child(3)').first().click();
+  await page.getByText('Logout').click();
+});
